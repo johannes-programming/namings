40e33d28da5239ab7c99a65a29e97ada1b9bd752
Johannes <johannes-programming@mailfence.com>
2026-05-25 20:36:41 +0200

    Version 1.0.0

--- src/namings/_utils/digest.py
+++ src/namings/_utils/digest.py
@@ -1,21 +1,28 @@
-from typing import *
+from collections.abc import Iterable
+from typing import Any, TypeVar, cast
+
+from ..typing.SupportsKeysAndGetitem import SupportsKeysAndGetitem
 
 __all__ = ["digest_data"]
+
+Value = TypeVar("Value")
 
 MISSING = object()
 
 
-def digest_data(data: Any, /) -> dict[str, Any]:
-    ans: dict[str, Any]
-    keys: Any
-    x: Any
-    y: Any
+def digest_data(
+    data: SupportsKeysAndGetitem[Value] | Iterable[tuple[object, Value]],
+    /,
+) -> dict[str, Value]:
+    ans: dict[str, Value]
+    x: object
+    y: Value
+    z: object
     ans = dict()
-    keys = getattr(type(data), "keys", MISSING)
-    if keys is MISSING:
+    if isinstance(data, SupportsKeysAndGetitem):
+        for z in data.keys():
+            ans[str(z)] = data[z]
+    else:
         for x, y in data:
             ans[str(x)] = y
-    else:
-        for x in keys(data):
-            ans[str(x)] = data[x]
     return ans
--- src/namings/typing/SupportsKeysAndGetitem.py
+++ src/namings/typing/SupportsKeysAndGetitem.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import Protocol, TypeVar
+from typing import Protocol, TypeVar, runtime_checkable
 
 import setdoc
 
@@ -9,6 +9,7 @@ __all__ = ["SupportsKeysAndGetitem"]
 Value = TypeVar("Value", covariant=True)
 
 
+@runtime_checkable
 class SupportsKeysAndGetitem(Protocol[Value]):
     @setdoc.basic
     def __getitem__(self, key: object, /) -> Value: ...
